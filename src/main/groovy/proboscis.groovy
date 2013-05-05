542883005360ecd3ff571b93de5f9760633f4a96
Dirk Gadsden <dirk@esherido.com>
2013-05-05 19:48:31 -0400

    Add main request and response classes

--- src/main/groovy/proboscis.groovy
+++ src/main/groovy/proboscis.groovy
@@ -1,7 +1,37 @@
 package co.langzeit.proboscis
 
-String test = "test"
-
-public class Test {
+public class Request {
+  String method
+  String format
+  int    length
+  byte[] data
   
+  Request() {
+    this.method = ""
+    this.format = ""
+    this.length = 0
+    this.data   = []
+  }
+  
+  Response makeResponse() {
+    Response r = new Response()
+    r.status = 200
+    r.format = this.format
+    return r
+  }
 }
+
+public class Response {
+  int    status
+  String format
+  int    length
+  byte[] data
+  
+  Response() {
+    this.status = 0
+    this.format = ""
+    this.length = 0
+    this.data   = []
+  }
+}
+
