11266a4120c30dbdea9a30bfd161a7b3e61bea61
Dirk Gadsden <dirk@esherido.com>
2013-05-06 10:19:06 -0400

    PoTCP: Add request decoding

--- src/main/groovy/potcp/coder.groovy
+++ src/main/groovy/potcp/coder.groovy
@@ -1,5 +1,30 @@
 package co.langzeit.proboscis.potcp
+import co.langzeit.proboscis.Request
+import co.langzeit.proboscis.Response
+
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.Scanner
+
+import groovy.transform.CompileStatic
 
 class Coder {
-  
+  @CompileStatic
+  static Request decodeRequest(InputStream input) {
+    Request req = new Request()
+    Scanner s   = new Scanner(input)
+    
+    s.useDelimiter(".")
+    req.method = s.next()
+    
+    s.useDelimiter(":")
+    req.format = s.next()
+    
+    String length = s.next()
+    req.length = length.toInteger()
+    
+    input.read(req.data, 0, req.length)
+    
+    return req
+  }
 }
