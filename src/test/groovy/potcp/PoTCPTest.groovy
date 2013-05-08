334d2b1eece3128468036589cd41dc3d336891a8
Dirk Gadsden <dirk@esherido.com>
2013-05-08 13:01:06 -0400

    PoTCP: Make test work

--- src/test/groovy/potcp/PoTCPTest.groovy
+++ src/test/groovy/potcp/PoTCPTest.groovy
@@ -3,7 +3,9 @@ import co.langzeit.proboscis.Request
 import co.langzeit.proboscis.Response
 
 import groovy.util.GroovyTestCase
+
 import org.junit.Test
+import org.junit.Assert
 
 import java.lang.Runnable
 
@@ -22,13 +24,15 @@ class ServerRunner implements Runnable {
   ServerRunner(Server server) { this.server = server }
   
   void run() {
-    server.bind(9000)
+    server.bind(PoTCPTest.PORT)
   }
 }
 
 class PoTCPTest extends GroovyTestCase {
   Server server
   Thread serverThread
+  
+  public static PORT = 9000
   
   PoTCPTest() {
     this.server = new Server()
@@ -42,5 +46,17 @@ class PoTCPTest extends GroovyTestCase {
     this.serverThread = new Thread(new ServerRunner(this.server))
     this.serverThread.start()
     System.out.println("Server listening")
+    
+    Client client = new Client("localhost", PoTCPTest.PORT)
+    
+    System.out.println("Client created; calling")
+    
+    String test_string = "Hello world!"
+    Response rep = client.call("echo", "text", test_string)
+    Assert.assertEquals(
+      "request and response data must be the same",
+      new String(rep.data), test_string
+    )
+    
   }
 }
